96694cf95d2369613b815c371532db3be37fb697
Aloïs Deniel <alois.deniel@gmail.com>
2022-05-24 15:53:44 +0200

    Added examples.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,115 +1,208 @@
+import 'package:fading_scroll/fading_scroll.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      title: 'Fading Scroll',
+      home: HomePage(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fading Scroll'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: ListView(
+        children: [
+          ...const <Example>[
+            VerticalListViewExample(),
+            PageViewExample(),
+            MixedViewExample(),
+          ].map(
+            (example) => ListTile(
+              title: Text(example.title),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(
+                        title: Text(example.title),
+                      ),
+                      body: example,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+abstract class Example extends Widget {
+  const Example({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  String get title;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class VerticalListViewExample extends StatefulWidget implements Example {
+  const VerticalListViewExample({
+    Key? key,
+    this.startColor = const Color(0xFFFF1FCE),
+    this.endColor = const Color(0xFFFFBB11),
+  }) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final Color startColor;
+  final Color endColor;
+
+  @override
+  String get title => 'Vertical ListView';
+
+  @override
+  State<VerticalListViewExample> createState() =>
+      _VerticalListViewExampleState();
+}
+
+class _VerticalListViewExampleState extends State<VerticalListViewExample>
+    with AutomaticKeepAliveClientMixin<VerticalListViewExample> {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return FadingScroll(
+      fadingSize: 200,
+      builder: (context, controller) {
+        return ListView(
+          controller: controller,
+          children: [
+            for (var i = 0; i < 100; i++)
+              Card(
+                child: ListTile(
+                  leading: Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: Color.lerp(
+                        const Color(0xFFFF1FCE),
+                        const Color(0xFFFFBB11),
+                        i / 100.0,
+                      ),
+                    ),
+                  ),
+                  title: Text('Color $i'),
+                ),
+              ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class PageViewExample extends StatefulWidget implements Example {
+  const PageViewExample({
+    Key? key,
+  }) : super(key: key);
+  @override
+  String get title => 'PageView';
+
+  @override
+  State<PageViewExample> createState() => _PageViewExampleState();
+}
+
+class _PageViewExampleState extends State<PageViewExample> {
+  late final PageController controller = PageController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    return FadingScroll(
+      controller: controller,
+      builder: (context, controller) {
+        return PageView(
+          controller: this.controller,
+          children: [
+            for (var i = 0; i < 6; i++)
+              Card(
+                child: ListView(
+                  children: [
+                    Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Color.lerp(
+                          const Color(0xFFFF1FCE),
+                          const Color(0xFFFFBB11),
+                          i / 6.0,
+                        ),
+                      ),
+                    ),
+                    Text('Color $i',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5),
+                  ],
+                ),
+              ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      },
+    );
+  }
+}
+
+class MixedViewExample extends StatefulWidget implements Example {
+  const MixedViewExample({
+    Key? key,
+  }) : super(key: key);
+  @override
+  String get title => 'Mixed Example';
+
+  @override
+  State<MixedViewExample> createState() => _MixedViewExampleState();
+}
+
+class _MixedViewExampleState extends State<MixedViewExample> {
+  late final PageController controller = PageController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadingScroll(
+      fadingSize: 100,
+      controller: controller,
+      builder: (context, controller) {
+        return PageView(
+          controller: this.controller,
+          children: [
+            for (var i = 0; i < 6; i++) const VerticalListViewExample(),
+          ],
+        );
+      },
     );
   }
 }
