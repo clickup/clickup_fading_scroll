bfa01fbc07d038d151bd99f0957c6be4cafc1416
aloisdeniel <alois.deniel@outlook.com>
2022-05-24 16:45:58 +0000

    Deploying to gh-pages from @ clickup/fading_scroll@0a3778ef4b028b73f4809ebdff142eade0d47190 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "667b3c5a30b5bad2a4fdbb9ed61d7777",
-"/": "667b3c5a30b5bad2a4fdbb9ed61d7777",
+"index.html": "be652cc15aa9f0395ef15d91da0d009f",
+"/": "be652cc15aa9f0395ef15d91da0d009f",
 "main.dart.js": "423195b25e161d170de4ecef122bb803",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
